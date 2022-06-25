8a15b68809b8e177c3b48eb79ae7b7e8aa43bd84
flamarionfagundes <flamarion.fagundes@ighthouseit.com.br>
2022-06-25 17:10:49 -0300

    feat: dynamic products with json

---
+++ js/script.js
@@ -0,0 +1,44 @@
+async function loadProducts() {
+  const response = await fetch("./products.json");
+  const products = await response.json();
+  return products;
+}
+
+function formatMoney(value) {
+  return value.toFixed(2).replace(".", ",");
+}
+
+function renderProductsHTML(imageThumbnailUrl, displayName, price) {
+  const productsContainer = document.querySelector("#products-container");
+  const productsHTML = `
+    <div class="product-container">
+    <label class="product-name-label-mobile" id="product-name-label">${displayName}</label>
+    <img class="product-image" id="product-image" src=${imageThumbnailUrl} alt="Imagem do produto ${displayName}" />
+    <div class="product-info">
+      <div class="product-name-wrapper">
+        <label class="product-name-label" id="product-name-label">${displayName}</label>
+        <img class="fav-icon-outline" src="./public/assets/images/fav-icon-outline.png"
+          alt="Favoritar produto" /> 
+      </div>
+      <div class="price-container-mobile">
+        <p class="price" id="price">R$${formatMoney(price)}</p>
+        <img class="fav-icon-outline fav-icon-outline-mobile" src="./public/assets/images/fav-icon-outline.png"
+        alt="Favoritar produto" />
+      </div>
+    </div>
+  `;
+  productsContainer.innerHTML += productsHTML;
+}
+
+function renderProducts(products) {
+  products.forEach(({ imageThumbnailUrl, displayName, price }) => {
+    renderProductsHTML(imageThumbnailUrl, displayName, price);
+  });
+}
+
+async function main() {
+  const products = await loadProducts();
+  renderProducts(products);
+}
+
+main();
